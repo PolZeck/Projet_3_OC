197952061cd09c24afc08a6d2ec6b1b483c535ff
Paul Ledieu <paul.ledieu@gmail.com>
2023-11-23 17:59:27 +0100

    update until 2.2 step

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,74 +1,81 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const buttonsContainer = document.querySelector(".buttons"); 
-  const gallery = document.querySelector('.gallery'); 
-  let worksData = []; 
+	  const buttonsContainer = document.querySelector(".buttons");
+	  const gallery = document.querySelector('.gallery');
+	  let worksData = [];
+	  let categories = [];
 
-  fetch('http://localhost:5678/api/works')
-    .then(response => response.json()) 
-    .then(data => {
-      worksData = data; 
-      displayGallery(worksData); 
-      createFilterButtons(); 
-    })
-    .catch(error => console.error('Erreur lors de la récupération des données de l\'API', error));
+	// Récupère les catégories depuis l'API
+	  fetch('http://localhost:5678/api/categories')
+		    .then(response => response.json())
+		    .then(data => {
+			      categories = data;
+			      createFilterButtons(); // Crée les boutons de filtre après avoir récupéré les catégories
+		})
+		    .catch(error => console.error('Erreur lors de la récupération des catégories', error));
 
-  function displayGallery(works) {
-    gallery.innerHTML = ""; 
-    for (let i = 0; i < works.length; i++) {
-      const work = works[i];
-      const figure = document.createElement('figure'); 
-      const img = document.createElement('img'); 
-      const figcaption = document.createElement('figcaption'); 
+	// Récupère les données des travaux depuis l'API
+	  fetch('http://localhost:5678/api/works')
+		    .then(response => response.json())
+		    .then(data => {
+			      worksData = data;
+			      displayGallery(worksData); // Affiche la galerie après avoir récupéré les travaux
+		    })
+		    .catch(error => console.error('Erreur lors de la récupération des travaux', error));
 
-      img.src = work.imageUrl; 
-      img.alt = work.title; 
-      figcaption.textContent = work.title; 
+	// Fonction pour afficher la galerie
+	  function displayGallery(works) {
+		    gallery.innerHTML = "";
+		    works.forEach(work => {
+			      const figure = document.createElement('figure');
+			      const img = document.createElement('img');
+			      const figcaption = document.createElement('figcaption');
 
-      figure.appendChild(img); 
-      figure.appendChild(figcaption); 
+			  img.src = work.imageUrl;
+			  img.alt = work.title;
+			  figcaption.textContent = work.title;
 
-      gallery.appendChild(figure); 
-    }
-  }
+			  figure.appendChild(img);
+			  figure.appendChild(figcaption);
 
-  function createFilterButtons() {
-    addButton("Tous", null); 
-    
-    const categoryIds = [1, 2, 3];
-    for (let i = 0; i < categoryIds.length; i++) {
-      const categoryId = categoryIds[i];
-      if (hasCategory(categoryId)) {
-        addButton(getCategoryName(categoryId), categoryId); 
-      }
-    }
-  }
+			  gallery.appendChild(figure);
+		});
+	}
 
-  function addButton(text, categoryId) {
-    const button = document.createElement("button"); 
-    button.textContent = text; 
-    button.addEventListener("click", () => filterGallery(categoryId)); 
-    buttonsContainer.appendChild(button); 
-  }
+	// Fonction pour créer les boutons de filtre
+	  function createFilterButtons() {
+		    addButton("Tous", null); // Ajoute le bouton "Tous"
 
-  function filterGallery(categoryId) {
-    const filteredWorks = (categoryId === null) ? worksData : worksData.filter(work => work.category.id === categoryId); 
-    displayGallery(filteredWorks); 
-  }
+		    categories.forEach(category => {
+			      addButton(category.name, category.id); // Ajoute un bouton pour chaque catégorie
+		});
+	}
 
-  function hasCategory(categoryId) {
-    return worksData.some(work => work.category.id === categoryId); 
-  }
+	// Fonction pour ajouter un bouton de filtre
+	  function addButton(text, categoryId) {
+		    const button = document.createElement("button");
+		    button.textContent = text;
+		    button.addEventListener("click", () => filterGallery(categoryId)); // Ajoute un gestionnaire d'événements pour filtrer au clic
+		    buttonsContainer.appendChild(button);
+	}
 
-  function getCategoryName(categoryId) {
-    switch (categoryId) {
-      case 1:
-        return "Objets";
-      case 2:
-        return "Appartements";
-      case 3:
-        return "Hôtels & Restaurants";
-      default:
-        return "Tous";
-    }
+	// Fonction pour filtrer la galerie en fonction de la catégorie sélectionnée
+	  function filterGallery(categoryId) {
+		    const filteredWorks = (categoryId === null) ? worksData : worksData.filter(work => work.category.id === categoryId);
+		    displayGallery(filteredWorks);
+	}
+});
+
+document.getElementById('connectBtn').addEventListener('click', function() {
+  // Récupération des valeurs des champs email et mot de passe
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+
+  // Vérification des identifiants et mots de passe
+  if (email === 'sophie.bluel@test.tld' && password === 'S0phie') {
+      // Si les identifiants sont corrects, redirection vers modif.html
+      window.location.href = 'modif.html';
+  } else {
+      // Si les identifiants sont incorrects, affichage du message d'erreur
+      alert('Erreur dans l’identifiant ou le mot de passe');
   }
 });
