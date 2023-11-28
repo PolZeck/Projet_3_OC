11e5ce5d46f535bbf2a88301b13a341b0fc121bf
Paul Ledieu <paul.ledieu@gmail.com>
2023-11-28 19:36:10 +0100

    update with api login

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,81 +1,114 @@
 document.addEventListener("DOMContentLoaded", function () {
-	  const buttonsContainer = document.querySelector(".buttons");
-	  const gallery = document.querySelector('.gallery');
-	  let worksData = [];
-	  let categories = [];
+  const buttonsContainer = document.querySelector(".buttons");
+  const gallery = document.querySelector('.gallery');
+  let worksData = [];
+  let categories = [];
 
-	// Récupère les catégories depuis l'API
-	  fetch('http://localhost:5678/api/categories')
-		    .then(response => response.json())
-		    .then(data => {
-			      categories = data;
-			      createFilterButtons(); // Crée les boutons de filtre après avoir récupéré les catégories
-		})
-		    .catch(error => console.error('Erreur lors de la récupération des catégories', error));
+  // Fonction pour afficher la galerie
+  function displayGallery(works) {
+    gallery.innerHTML = "";
+    works.forEach(work => {
+      const figure = document.createElement('figure');
+      const img = document.createElement('img');
+      const figcaption = document.createElement('figcaption');
 
-	// Récupère les données des travaux depuis l'API
-	  fetch('http://localhost:5678/api/works')
-		    .then(response => response.json())
-		    .then(data => {
-			      worksData = data;
-			      displayGallery(worksData); // Affiche la galerie après avoir récupéré les travaux
-		    })
-		    .catch(error => console.error('Erreur lors de la récupération des travaux', error));
+      img.src = work.imageUrl;
+      img.alt = work.title;
+      figcaption.textContent = work.title;
 
-	// Fonction pour afficher la galerie
-	  function displayGallery(works) {
-		    gallery.innerHTML = "";
-		    works.forEach(work => {
-			      const figure = document.createElement('figure');
-			      const img = document.createElement('img');
-			      const figcaption = document.createElement('figcaption');
+      figure.appendChild(img);
+      figure.appendChild(figcaption);
 
-			  img.src = work.imageUrl;
-			  img.alt = work.title;
-			  figcaption.textContent = work.title;
+      gallery.appendChild(figure);
+    });
+  }
 
-			  figure.appendChild(img);
-			  figure.appendChild(figcaption);
+  // Fonction pour créer les boutons de filtre
+  function createFilterButtons() {
+    addButton("Tous", null);
 
-			  gallery.appendChild(figure);
-		});
-	}
+    categories.forEach(category => {
+      addButton(category.name, category.id);
+    });
+  }
 
-	// Fonction pour créer les boutons de filtre
-	  function createFilterButtons() {
-		    addButton("Tous", null); // Ajoute le bouton "Tous"
+  // Fonction pour ajouter un bouton de filtre
+  function addButton(text, categoryId) {
+    const button = document.createElement("button");
+    button.textContent = text;
+    button.addEventListener("click", () => filterGallery(categoryId));
+    buttonsContainer.appendChild(button);
+  }
 
-		    categories.forEach(category => {
-			      addButton(category.name, category.id); // Ajoute un bouton pour chaque catégorie
-		});
-	}
+  // Fonction pour filtrer la galerie en fonction de la catégorie sélectionnée
+  function filterGallery(categoryId) {
+    const filteredWorks = (categoryId === null) ? worksData : worksData.filter(work => work.category.id === categoryId);
+    displayGallery(filteredWorks);
+  }
 
-	// Fonction pour ajouter un bouton de filtre
-	  function addButton(text, categoryId) {
-		    const button = document.createElement("button");
-		    button.textContent = text;
-		    button.addEventListener("click", () => filterGallery(categoryId)); // Ajoute un gestionnaire d'événements pour filtrer au clic
-		    buttonsContainer.appendChild(button);
-	}
+  // Récupère les catégories depuis l'API
+  fetch('http://localhost:5678/api/categories')
+    .then(response => response.json())
+    .then(data => {
+      categories = data;
+      createFilterButtons();
+    })
+    .catch(error => console.error('Erreur lors de la récupération des catégories', error));
 
-	// Fonction pour filtrer la galerie en fonction de la catégorie sélectionnée
-	  function filterGallery(categoryId) {
-		    const filteredWorks = (categoryId === null) ? worksData : worksData.filter(work => work.category.id === categoryId);
-		    displayGallery(filteredWorks);
-	}
+  // Récupère les données des travaux depuis l'API
+  fetch('http://localhost:5678/api/works')
+    .then(response => response.json())
+    .then(data => {
+      worksData = data;
+      displayGallery(worksData);
+    })
+    .catch(error => console.error('Erreur lors de la récupération des travaux', error));
+
+  // Vérifier si l'utilisateur est connecté lors du chargement de la page
+  const loggedIn = localStorage.getItem('loggedIn');
+  if (loggedIn !== 'true') {
+    const filterSection = document.querySelector('.filter-container'); // Utilise .filter-container au lieu de .filter
+    if (filterSection) {
+      filterSection.style.display = 'block'; // Affiche la partie .filter-container si l'utilisateur n'est pas connecté
+    }
+  }
 });
 
-document.getElementById('connectBtn').addEventListener('click', function() {
-  // Récupération des valeurs des champs email et mot de passe
-  const email = document.getElementById('email').value;
-  const password = document.getElementById('password').value;
+document.getElementById('connectBtn').addEventListener('click', async function () {
+  const emailInput = document.getElementById('email');
+  const passwordInput = document.getElementById('password');
+  const errorMessage = document.getElementById('errorMessage');
 
-  // Vérification des identifiants et mots de passe
-  if (email === 'sophie.bluel@test.tld' && password === 'S0phie') {
-      // Si les identifiants sont corrects, redirection vers modif.html
-      window.location.href = 'modif.html';
-  } else {
-      // Si les identifiants sont incorrects, affichage du message d'erreur
-      alert('Erreur dans l’identifiant ou le mot de passe');
+  const email = emailInput.value;
+  const password = passwordInput.value;
+
+  try {
+    const response = await fetch('http://localhost:5678/api/users/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        email: email,
+        password: password
+      })
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      if (data.token) {
+        // Redirection vers index.html
+        window.location.href = 'index.html';
+
+        // Suppression de la partie filter sur la page index.html
+        localStorage.setItem('loggedIn', 'true'); // Enregistrer dans localStorage que l'utilisateur est connecté
+      }
+    } else {
+      errorMessage.style.display = 'block';
+      emailInput.style.border = '2px solid red';
+      passwordInput.style.border = '2px solid red';
+    }
+  } catch (error) {
+    console.error('Erreur :', error);
   }
 });
