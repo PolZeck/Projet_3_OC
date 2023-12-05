635a7e660c1f1d27a0f520080a5472959576d09d
Paul Ledieu <paul.ledieu@gmail.com>
2023-12-05 17:48:06 +0100

    update with mentor's recommendation

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -4,6 +4,7 @@ document.addEventListener("DOMContentLoaded", function () {
   let worksData = [];
   let categories = [];
 
+
   // Fonction pour afficher la galerie
   function displayGallery(works) {
     gallery.innerHTML = "";
@@ -66,18 +67,113 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 });
+
+
+          // adminPart
+
+
 const loggedIn = localStorage.getItem('loggedIn');
+const filterSection = document.querySelector('.filter-container');
+const logoutButton = document.createElement('li');
+logoutButton.textContent = 'logout';
+logoutButton.style.cursor = 'pointer';
+const editionMode = document.getElementById('editionMode');
+const modalButton = document.getElementById('modal');
+
+if (loggedIn === 'true') {
+  const loginButton = document.querySelector('#loginButton');
+
+  if (loginButton) {
+    loginButton.replaceWith(logoutButton);
+  }
+
+  if (filterSection) {
+    filterSection.remove();
+  }
+
+  if (editionMode) {
+    editionMode.style.display = 'block';
+  }
+
+  if (modalButton) {
+    modalButton.style.display = 'block';
+  }
+
+  //déconnexion :
+  logoutButton.addEventListener('click', function () {
+    localStorage.removeItem('loggedIn');
+    window.location.href = 'index.html';
+  });
+}
 
 
+          // modalPart
+
+document.addEventListener("DOMContentLoaded", function () {
+  const galleryModal = document.getElementById('galleryModal');
+  const closeModalSpan = document.querySelector('.close');
+  const galleryPhotos = document.querySelector('.gallery-photos');
+  const modalButton = document.querySelector('#modal');
+  
+  
+
+  // Gestion de l'ouverture de la boîte modale
+  modalButton.addEventListener('click', function () {
+      galleryModal.style.display = 'block';
+      loadGalleryPhotos(); // Charger les photos depuis l'API dans la galerie
+  });
+
+  // Gestion de la fermeture de la boîte modale
+  closeModalSpan.addEventListener('click', function () {
+      galleryModal.style.display = 'none';
+  });
+
+  // Fermer la boîte modale lorsque l'utilisateur clique en dehors de celle-ci
+  window.addEventListener('click', function (event) {
+      if (event.target === galleryModal) {
+          galleryModal.style.display = 'none';
+      }
+  });
+
+  // Fonction pour charger les photos depuis l'API dans la galerie
+  async function loadGalleryPhotos() {
+      try {
+          const response = await fetch('http://localhost:5678/api/works');
+          if (!response.ok) {
+              throw new Error('Erreur lors de la récupération des photos');
+          }
+          const worksData = await response.json();
+
+          // Effacer les images précédentes
+          galleryPhotos.innerHTML = '';
+
+          worksData.forEach(work => {
+              const img = document.createElement('img');
+              img.src = work.imageUrl;
+              img.alt = work.title;
+              img.classList.add('gallery-photo');
+              img.addEventListener('click', function () {
+                  openFullImage(work.imageUrl);
+              });
+              galleryPhotos.appendChild(img);
+          });
+      } catch (error) {
+          console.error('Erreur :', error);
+      }
+  }
+
+
+});
+
+      // connexionPart
+      
 document.getElementById('connectBtn').addEventListener('click', async function () {
-  const emailInput = document.getElementById('email');
-  const passwordInput = document.getElementById('password');
-  const errorMessage = document.getElementById('errorMessage');
+    const emailInput = document.getElementById('email');
+    const passwordInput = document.getElementById('password');
+    const errorMessage = document.getElementById('errorMessage');
 
-
-
-  const email = emailInput.value;
-  const password = passwordInput.value;
+    const email = emailInput.value;
+    const password = passwordInput.value;
 
   try {
     const response = await fetch('http://localhost:5678/api/users/login', {
@@ -95,7 +191,10 @@ document.getElementById('connectBtn').addEventListener('click', async function (
       const data = await response.json();
       if (data.token) {
         localStorage.setItem('loggedIn', 'true');
-        window.location.href = 'index.html'; // Redirection vers index.html
+        localStorage.setItem('token', data.token); 
+        window.location.href = 'index.html';
+        // Redirection vers index.html
+
       }
     } else {
       errorMessage.style.display = 'block';
@@ -105,4 +204,5 @@ document.getElementById('connectBtn').addEventListener('click', async function (
   } catch (error) {
     console.error('Erreur :', error);
   }
+
 });
